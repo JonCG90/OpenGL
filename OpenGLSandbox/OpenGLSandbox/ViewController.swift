795a23de788ffd4fbe6f9154cea9f9877ac0fcf8
Jon Graham <JonCG90@gmail.com>
2014-12-07 20:57:09 -0800

    Create project file

---
+++ OpenGLSandbox/OpenGLSandbox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OpenGLSandbox
+//
+//  Created by Jonathan Graham on 12/7/14.
+//  Copyright (c) 2014 Jonathan Graham. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
